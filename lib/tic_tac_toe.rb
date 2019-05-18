e7a11549fae1718c046d9e2814937c4c31b9e515
Ashley Muller <serenehopes@gmail.com>
2019-05-18 06:01:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,9 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp!
   index = input_to_index(input)
+  if !valid_move(board, index)
+    turn(board)
+  end
   move(board, index, current_player(board))
 end
 
